0f0a9fb64c322b3dc7f497cc54947be16fce5e8b
arjunverma06 <arjunverma5319@gmail.com>
2024-09-16 12:22:48 +0530

    okk

--- arjun.py
+++ arjun.py
@@ -18,13 +18,9 @@ engine = pyttsx3.init("sapi5")
 voices = engine.getProperty("voices")
 # print(voices[1].id)
 engine.setProperty("voice", voices[1].id)
-
-
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
-
-
 def wishMe():
     hour = int(datetime.datetime.now().hour)
     if hour >= 0 and hour < 12:
