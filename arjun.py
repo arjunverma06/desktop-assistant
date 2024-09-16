1729fce796f0f93e250853a77f1ef6312ced2b1d
arjunverma06 <arjunverma5319@gmail.com>
2024-09-16 12:10:22 +0530

    ok

--- arjun.py
+++ arjun.py
@@ -9,6 +9,10 @@ import operator
 import ctypes
 import wolframalpha
 import pyjokes
+import win32com.client #this for text to speak coverted
+import openai
+from ecapture import ecapture as ec
+# from config import apikey
 # from ecapture import ecapture as ec
 engine = pyttsx3.init("sapi5")
 voices = engine.getProperty("voices")
@@ -32,16 +36,54 @@ def wishMe():
     else:
         speak("Good Evening!")
 
-    speak("I am   assistant of arjun verma. Please tell me how may I help you")
+    speak("I am  your virtual assistant . Please tell me how may I help you")
+    
+# def sendEmail(to, content):
+#     server = smtplib.SMTP('smtp.gmail.com', 587)
+#     server.ehlo()
+#     server.starttls()
+     
+#     # Enable low security in gmail
+#     server.login('arjun225553@gamil.com', '')
+#     server.sendmail('arjun225553@gamil.com', to, content)
+#     server.close() 
+def ai(prompt):
+     openai.api_key = apikey
+     text = f"OpenAI response for Prompt: {prompt} \n *************************\n\n"
 
+     response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    
+    # todo: Wrap this inside of a  try catch block
+    # print(response["choices"][0]["text"])
+     text += response["choices"][0]["text"]
+     if not os.path.exists("Openai"):
+        os.mkdir("Openai")
 
+    # with open(f"Openai/prompt- {random.randint(1, 2343434356)}", "w") as f:
+     with open(f"Openai/{''.join(prompt.split('intelligence')[1:]).strip() }.txt", "w") as f:
+        f.write(text)
+
+def say(text):
+    os.system(f'say "{text}"')
+
+  
+
+    
 def takeCommand():
     # It takes microphone input from the user and returns string output
 
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
-        r.pause_threshold = 1
+        r.pause_threshold = 0.5
         audio = r.listen(source)
 
     try:
@@ -54,15 +96,16 @@ def takeCommand():
         print("Say that again please...")   #Say that again will be printed in case of improper voice 
         return "None" #None string will be returned
     return query
+    
+    speaker=win32com.client.Dispatch("SAPI.SpVoice") 
+    while 1:
+       print("entert the word") 
+       s=input()
+       speaker.Speak(s)
 
 
-def sendEmail(to, content):
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.ehlo()
-    server.starttls()
-    server.login("arjun225553@gmail.com", "csahxdncmmniojpi")
-    server.sendmail("arjun225553@gmail.com", to, content)
-    server.close()
+
+
 
 
 if __name__ == "__main__":
@@ -91,7 +134,7 @@ if __name__ == "__main__":
         elif "open stack overflow" in query:
             webbrowser.open("stackoverflow.com")
         elif "play music" in query:
-            music_dir = "E:\SONG"
+            music_dir = "E:\\Mp3 song\\Mp3 song"
             songs = os.listdir(music_dir)
             print(songs)
             os.startfile(os.path.join(music_dir, songs[0]))
@@ -103,25 +146,17 @@ if __name__ == "__main__":
             codePath = "C:\\Program Files\\Microsoft VS Code\\Code.exe"
             os.startfile(codePath)
         elif "shutdown" in query:
+            speak("Hold On a Sec ! Your system is on its way to shut down")
             os_dir = "C:\\Users\\Dell\\OneDrive\\Desktop\\arjun"
             
             shutdown = os.listdir(os_dir)
             print(shutdown)
             os.startfile(os.path.join(os_dir, shutdown[0]))
+        elif "Using artificial intelligence".lower() in query.lower():
+            ai(prompt=query)
 
         elif "search" in query:
             webbrowser.open(f"www.google.com/search?q={query.replace('search','')}")
-
-        # elif 'email to arjun' in query:
-        # try:
-        # speak("What should I say?")
-        # content = takeCommand()
-        #   to = "arjun225553@gmail.com"
-        # sendEmail(to, content)
-        #  speak("Email has been sent!")
-        # except Exception as e:
-        # print(e)
-        # speak("Sorry my friend  arjun bhai. I am not able to send this email")
 
         elif "how are you" in query:
             speak("I am fine, Thank you sir")
@@ -130,10 +165,11 @@ if __name__ == "__main__":
 
         elif "sketch design" in query:
             speak("ok i am draw the sketch  design sir")
-            os_dir = "D:\\Majar project folder\\TASLLEM SIR PROJECT _voice_asistance\\radhakrishna code"
+            os_dir = "D:\\Majar project folder\\TASLLEM SIR PROJECT _voice_asistance\\radhakrishna code\\"
             design = os.listdir(os_dir)
             print(design)
             os.startfile(os.path.join(os_dir, design[0]))
+           
             
         elif 'lock window' in query:
                 speak("locking the device")
@@ -148,3 +184,39 @@ if __name__ == "__main__":
             speak(pyjokes.get_joke())
         elif "camera" in query or "take a photo" in query:
             ec.capture(0, "Jarvis Camera ", "img.jpg")
+        elif "open" in query:
+             webbrowser.open("chat.openai.com")  
+        # elif 'send a mail' in query:
+        #     try:
+        #         speak("What should I say?")
+        #         content = takeCommand()
+        #         speak("whome should i send")
+        #         to = input()    
+        #         sendEmail(to, content)
+        #         speak("Email has been sent !")
+        #     except Exception as e:
+        #         print(e)
+        #         speak("I am not able to send this email") 
+        elif "change my name to" in query:
+            query = query.replace("change my name to", "")
+            assname = query 
+            
+        elif "change name" in query:
+            speak("What would you like to call me, Sir ")
+            assname = takeCommand()
+            speak("Thanks for naming me")
+        elif "who made you" in query or "who created you" in query: 
+            speak("I have been created by Arjun Abhishek Aniket.")
+        elif 'powerpoint presentation' in query:
+            speak("opening Power Point presentation")
+            power = "C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs"
+            os.startfile(power) 
+        elif 'change background' in query:
+            ctypes.windll.user32.SystemParametersInfoW(20, 
+                                                       0, 
+                                                       "https://images.pexels.com/photos/33109/fall-autumn-red-season.jpg?auto=compress&cs=tinysrgb&w=600.png",
+                                                       0)
+            speak("Background changed successfully")
+
+
+        
